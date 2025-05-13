31b67fc0446397d1336dfbf82e0f851756abc049
Denidu Gamage <denidugamage21@gmail.com>
2025-05-13 13:48:53 +0530

    Initial Commit

---
+++ SwiftPushNotification/SwiftPushNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPushNotificationApp.swift
+//  SwiftPushNotification
+//
+//  Created by Denidu Gamage on 2025-05-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPushNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftPushNotificationTests/SwiftPushNotificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPushNotificationTests.swift
+//  SwiftPushNotificationTests
+//
+//  Created by Denidu Gamage on 2025-05-13.
+//
+
+import Testing
+@testable import SwiftPushNotification
+
+struct SwiftPushNotificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
